0120d65c3ba3d7ee9977f9ca51a7d732e2a2a2ec
M <132958332+Mgithoob@users.noreply.github.com>
2025-03-21 18:31:24 -0300

    Update Fila.java

--- TDE1_resProbEstrut/src/Fila.java
+++ TDE1_resProbEstrut/src/Fila.java
@@ -1,2 +1,19 @@
 public class Fila {
+    private int inicio;
+    private int fim;
+    private int MAX;
+
+    int dados[];
+
+    public Fila(int tamanho) {
+        dados = new int[tamanho];
+        inicio = 0;
+        fim = 0;
+    }
+
+    public inserir(){
+
+    }
+
+
 }
