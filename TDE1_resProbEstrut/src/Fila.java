5e100e93ac0f768f78a74ab32f46fa62f724c948
M <132958332+Mgithoob@users.noreply.github.com>
2025-03-23 22:16:33 -0300

    Adc final int MAX

--- TDE1_resProbEstrut/src/Fila.java
+++ TDE1_resProbEstrut/src/Fila.java
@@ -1,50 +1,58 @@
 public class Fila {
     private int inicio;
     private int fim;
+    private final int MAX;
 
     private int[] dados;
 
-    public Fila(int tamanho) {
-        dados = new int[tamanho];
+    public Fila(int MAX) {
+        this.MAX = MAX;
+        dados = new int[MAX];
         inicio = 0;
         fim = 0;
     }
 
     // Insere objeto no começo da fila
     public void inserir(int elemento){
+        try {
+            // caso para inicio
+            if (fim <= 0) dados[fim] = elemento;
+            else if (cheia()) {
+                // exceção fila cheia
+                throw new FilaCheiaException();
+                // TODO: catch
+                // wrap
+            } else if ((fim + 1) >= MAX){
+                dados[fim] = elemento;
+                fim = 0;
+            } else {
+                // caso geral
+                dados[fim] = elemento;
+                fim++;
+            }
+        } catch (FilaCheiaException e) {
 
-        // caso para inicio
-        if (fim <= 0) dados[fim] = elemento;
-        else if (cheia()) {
-            // exceção fila cheia
-            throw new FilaCheiaException();
-            // TODO: catch
-        // wrap
-        } else if ((fim + 1) >= dados.length){
-            dados[fim] = elemento;
-            fim = 0;
-        } else {
-            // caso geral
-            dados[fim] = elemento;
-            fim++;
         }
+
     }
 
     // Retira objeto do fim da fila
     public int retirar(){
         int elemento;
 
-        if (vazia()) {
-            throw new FilaVaziaException();
+        try {
+            if (vazia()) throw new FilaVaziaException();
+            else {
+                elemento = dados[inicio];
+                // wrap
+                if ( (inicio + 1) >= MAX) inicio = 0;
+                else inicio++;
+
+                return elemento;
+            }
+        } catch (FilaVaziaException e){
+            // TODO: lidar com exceção
             return -1;
-        } else {
-            elemento = dados[inicio];
-
-            // wrap
-            if ( (inicio + 1) >= dados.length ) inicio = 0;
-            else inicio++;
-
-            return elemento;
         }
     }
 
